c57c322b1d3c492609f7162165d34083652c392b
RizwAnsari <rizwan.na@outlook.com>
2024-01-13 18:05:47 +0530

    install and configure express server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import express, { Express, Request, Response } from "express";
+import 'dotenv/config'
+
+const app: Express = express();
+const PORT: Number = parseInt(process.env.APP_PORT || "3000");
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, this is Express + TypeScript");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${process.env.APP_URL}:${PORT}`);
+});
